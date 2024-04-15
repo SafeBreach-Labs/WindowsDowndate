f79ce6a520f24ca4e306dafc117b4f9864902dec
Alon Leviev <alon.leviev@safebreach.com>
2024-04-15 18:14:13 +0300

    Added logger

--- windows_downdate.py
+++ windows_downdate.py
@@ -1,8 +1,29 @@
 import logging
+import os
+
+logger = logging.getLogger(__name__)
 
 
-def main():
-    pass
+CONFIG_FILE_PATH = "resources/Config.xml"
+
+
+def init_logger() -> None:
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    log_format = logging.Formatter('[%(levelname)s]: %(message)s')
+    stream_handler.setFormatter(log_format)
+    logger.addHandler(stream_handler)
+
+
+def parse_config_file() -> None:
+    if not os.path.exists(CONFIG_FILE_PATH):
+        raise FileNotFoundError("Config file Config.xml does not exist")
+
+
+def main() -> None:
+    init_logger()
+
+    parse_config_file()
 
 
 if __name__ == '__main__':
