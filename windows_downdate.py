ba0c9b7945227f18db4ba04f96fca1a1210395d0
Alon Leviev <alon.leviev@safebreach.com>
2024-04-17 13:08:16 +0300

    Minimized parse_config_xml

--- windows_downdate.py
+++ windows_downdate.py
@@ -1,36 +1,48 @@
 import logging
+from dataclasses import dataclass
+from typing import List
 
 from utils.filesystem import is_path_exists, Path
-from utils.xml_utils import load_xml, find_child_elements_by_match, get_element_attribute, create_element, \
-    append_child_element
-from utils.component_store import retrieve_oldest_file_by_file_path
+from utils.xml_utils import load_xml, find_child_elements_by_match, get_element_attribute
 
 logger = logging.getLogger(__name__)
 
 
-CONFIG_FILE_PATH = "resources/Config.xml"
+CONFIG_XML_PATH = "resources/Config.xml"
 PENDING_XML_PATH = "resources/Pending.xml"
 DOWNGRADE_XML_PATH = "resources/Downgrade.xml"
+
+
+@dataclass
+class UpdateFile:
+
+    source: Path
+    destination: Path
+    should_retrieve_oldest: bool
+    is_oldest_retrieved: bool
+
+    def to_hardlink_dict(self):
+        return {"source": self.source.nt_path, "destination": self.destination.nt_path}
+
+
+def parse_args() -> None:
+    pass
 
 
 def init_logger() -> None:
     logger.setLevel(logging.INFO)
     stream_handler = logging.StreamHandler()
-    log_format = logging.Formatter('[%(levelname)s]: %(message)s')
+    log_format = logging.Formatter('[%(levelname)s] %(message)s')
     stream_handler.setFormatter(log_format)
     logger.addHandler(stream_handler)
 
 
-def parse_config_xml() -> None:
+def parse_config_xml() -> List[UpdateFile]:
 
-    pending_xml = load_xml(PENDING_XML_PATH)
-    poq_element = find_child_elements_by_match(pending_xml, "./POQ")[0]  # Post reboot POQ is always at index 0
+    config_xml = load_xml(CONFIG_XML_PATH)
 
-    config_xml = load_xml(CONFIG_FILE_PATH)
-
+    update_files = []
     for update_file in find_child_elements_by_match(config_xml, "./UpdateFilesList/UpdateFile"):
-        source_file = get_element_attribute(update_file, "source")
-        source_file_obj = Path(source_file)
         destination_file = get_element_attribute(update_file, "destination")
         destination_file_obj = Path(destination_file)
 
@@ -38,24 +50,25 @@ def parse_config_xml() -> None:
         if not is_path_exists(destination_file_obj.full_path):
             raise FileNotFoundError(f"The file to update {destination_file_obj.full_path} does not exist")
 
+        source_file = get_element_attribute(update_file, "source")
+        source_file_obj = Path(source_file)
+
         # If the source does not exist, retrieve its oldest version from the component store
         if not is_path_exists(source_file_obj.full_path):
-            retrieve_oldest_file_by_file_path(destination_file_obj, source_file_obj)
+            should_retrieve_oldest = True
+        else:
+            should_retrieve_oldest = False
 
-        hardlink_dict = {"source": source_file_obj.nt_path, "destination": destination_file_obj.nt_path}
-        hardlink_element = create_element("HardlinkFile", hardlink_dict)
-        append_child_element(poq_element, hardlink_element)
+        update_file_obj = UpdateFile(source_file_obj, destination_file_obj, should_retrieve_oldest, False)
+        update_files.append(update_file_obj)
 
-    pending_xml.write(DOWNGRADE_XML_PATH)
-
-
-def parse_args() -> None:
-    pass
+    return update_files
 
 
 def main() -> None:
+    parse_args()
     init_logger()
-    parse_config_xml()
+    update_files = parse_config_xml()
 
 
 if __name__ == '__main__':
