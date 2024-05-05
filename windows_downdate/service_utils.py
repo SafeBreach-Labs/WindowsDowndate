0b1c19388610215bdcbbb49f81b609071d988d5a
Alon Leviev <alon.leviev@safebreach.com>
2024-05-05 22:11:40 +0300

    Added start_service and query_service_status to service_utils.py

--- windows_downdate/service_utils.py
+++ windows_downdate/service_utils.py
@@ -1,4 +1,25 @@
+import time
+from dataclasses import dataclass
+from typing import List, Any
+
 import win32service
+import win32serviceutil
+
+
+SERVICE_PENDING_STATES = [win32service.SERVICE_START_PENDING, win32service.SERVICE_STOP_PENDING,
+                          win32service.SERVICE_CONTINUE_PENDING, win32service.SERVICE_PAUSE_PENDING]
+WAIT_FOR_STATUS_TIMEOUT = 20
+
+
+@dataclass
+class ServiceStatus:
+    service_type: int
+    current_state: int
+    controls_accepted: int
+    win32_exit_code: int
+    service_specific_exit_code: int
+    check_point: int
+    wait_hing: int
 
 
 def set_service_start_type(service_name: str, start_type: int) -> None:
@@ -18,3 +39,32 @@ def set_service_start_type(service_name: str, start_type: int) -> None:
         None
     )
 
+
+def query_service_status(service_name: str) -> ServiceStatus:
+    service_status = win32serviceutil.QueryServiceStatus(service_name)
+    return ServiceStatus(*service_status)
+
+
+def start_service(service_name: str, service_args: List[Any] = None, resume_if_paused: bool = True) -> None:
+    service_status = query_service_status(service_name)
+
+    while service_status.current_state in SERVICE_PENDING_STATES:
+        time.sleep(10.0)
+        service_status = query_service_status(service_name)
+
+    if service_status.current_state == win32service.SERVICE_RUNNING:
+        return
+
+    elif service_status.current_state == win32service.SERVICE_PAUSED:
+        if resume_if_paused:
+            win32serviceutil.ControlService(service_name, win32service.SERVICE_CONTROL_CONTINUE)
+        else:
+            return
+
+    elif service_status.current_state == win32service.SERVICE_STOPPED:
+        win32serviceutil.StartService(service_name, service_args)
+
+    else:
+        raise Exception(f"Unexpected service state: {service_status.current_state}")
+
+    win32serviceutil.WaitForServiceStatus(service_name, win32service.SERVICE_RUNNING, WAIT_FOR_STATUS_TIMEOUT)
