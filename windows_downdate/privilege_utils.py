e194b0f258fb07a4a323e1f9e347f89f08fb95eb
Alon Leviev <alon.leviev@safebreach.com>
2024-05-05 21:11:01 +0300

    Rename process_utils.py's get_process_by_name to get_process_id_by_name

--- windows_downdate/privilege_utils.py
+++ windows_downdate/privilege_utils.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
 
 import win32api
+import win32con
 import win32security
+
+from windows_downdate.process_utils import get_process_id_by_name
 
 
 def convert_privilege_name_to_luid(privilege: Tuple[str, int]) -> Tuple[int, int]:
@@ -26,3 +29,19 @@ def enable_backup_privilege() -> None:
 def enable_restore_privilege() -> None:
     privilege = [(win32security.SE_RESTORE_NAME, win32security.SE_PRIVILEGE_ENABLED)]
     adjust_token_privileges(privilege)
+
+
+def impersonate_process_by_process_id(process_id: int) -> None:
+    process_handle = win32api.OpenProcess(win32con.PROCESS_QUERY_LIMITED_INFORMATION, False, process_id)
+    process_token_handle = win32security.OpenProcessToken(process_handle, win32con.TOKEN_DUPLICATE)
+    dup_process_token_handle = win32security.DuplicateTokenEx(process_token_handle,
+                                                              win32con.MAXIMUM_ALLOWED,
+                                                              None,
+                                                              win32security.SecurityImpersonation,
+                                                              win32security.TokenImpersonation)
+    win32security.ImpersonateLoggedOnUser(dup_process_token_handle)
+
+
+def impersonate_nt_system() -> None:
+    winlogon_pid = get_process_id_by_name("winlogon.exe")
+    impersonate_process_by_process_id(winlogon_pid)
