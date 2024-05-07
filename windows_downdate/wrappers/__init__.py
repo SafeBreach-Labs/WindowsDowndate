e78b9cb4acc72efafa4a51fc1aa4a59c6be33529
Alon Leviev <alon.leviev@safebreach.com>
2024-05-07 14:59:08 +0300

    Add helpers to wrappers __init__.py

--- windows_downdate/wrappers/__init__.py
+++ windows_downdate/wrappers/__init__.py
@@ -1,8 +1,30 @@
 import ctypes
-from typing import Callable, Tuple, Any
+from ctypes import wintypes
+from typing import Callable, Tuple, Any, Union
+
+import pywintypes
+
+
+#############
+# Constants #
+#############
+
+
+NULL_PTR = wintypes.LPVOID(0)
+
+
+####################
+# Helper functions #
+####################
 
 
 def raise_if_false(result: int, func: Callable = None, arguments: Tuple[Any] = ()) -> int:
     if not result:
         raise ctypes.WinError(result, "Error message here")  # TODO: Add FormatMessage
     return result
+
+
+def convert_pyhandle_to_handle(token_handle: Union[int, pywintypes.HANDLE]) -> int:
+    if isinstance(token_handle, pywintypes.HANDLEType):
+        token_handle = token_handle.handle
+    return token_handle
